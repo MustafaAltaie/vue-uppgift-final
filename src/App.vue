23d07652e47e47c7de21709f5713807bb18b6537
mustafa altaie <mustafaphoto111@gmail.com>
2026-01-31 16:13:12 +0100

    Changed style, added responsiveness

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <!-- src>App.vue -->
 <template>
-  <nav>
+  <nav class="navHeader">
     <button @click="toggleTheme" class="darkToggle">
       {{ darkMode ? 'Light Mode' : 'Dark Mode' }}
     </button>
@@ -18,6 +18,9 @@ const { darkMode, toggleTheme } = useDarkMode();
 </script>
 
 <style>
+  * {
+    box-sizing: border-box;
+  }
   :root {
     --bg-color: white;
     --text-color: black;
@@ -33,20 +36,52 @@ const { darkMode, toggleTheme } = useDarkMode();
     background-color: var(--bg-color);
     color: var(--text-color);
   }
+  .navHeader {
+    position: sticky;
+    top: 0;
+    z-index: 100;
+    background-color: #fff;
+  }
+  .dark .navHeader {
+    background-color: #121212;
+  }
   .darkToggle {
     margin-right: 10px;
+    border-radius: 20px;
+    padding: 10px;
+    cursor: pointer;
   }
   nav {
     display: flex;
     align-items: center;
-    gap: 10px;
+    gap: 30px;
     border-bottom: solid 1px;
     padding: 10px;
+  }
+  nav > a {
+    color: black;
+    text-decoration: none;
+  }
+  .dark nav > a {
+    color: white;
+  }
+  .dark button {
+    background-color: rgb(7, 85, 163);
+    color: white;
+    border: none;
+  }
+  button {
+    border-radius: 20px;
+    border: none;
   }
 
   @media (max-width: 600px) {
     .grid {
       flex-direction: column;
+    }
+    nav {
+      gap: 0;
+      justify-content: space-between;
     }
   }
 </style>
